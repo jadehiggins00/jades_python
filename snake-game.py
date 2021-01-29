b49cf0b551cf83093e70c69f5f05830bbd0d55dc
jadehiggins00 <jadehiggins2000@gmail.com>
2021-01-29 20:02:23 +0000

    version 1.1

---
+++ snake-game.py
@@ -0,0 +1,14 @@
+# this is a snake game
+# author: jade higgins
+# version: 1.1
+
+import pygame # importing the pygame library
+# this intialises all the imported pygame modules, used at the start
+pygame.init()
+# creating the screen and setting the size
+dis = pygame.display.set_model((400, 300))
+# the update part is used to update any changes made to the screen
+pygame.display.update()
+# the quit() is used to unintialise everything at the end of the code
+pygame.quit()
+quit()
