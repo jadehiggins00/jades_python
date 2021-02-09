cda45e64b2a5b9717993f5c1a4074656a3b43f97
jadehiggins00 <jadehiggins2000@gmail.com>
2021-02-09 22:31:28 +0000

    intial commit

--- snake-game.py
+++ snake-game.py
@@ -9,6 +9,11 @@ pygame.init()
 dis = pygame.display.set_model((400, 300))
 # the update part is used to update any changes made to the screen
 pygame.display.update()
+# setting the title of the window to a caption
+pygame.display.set_caption('Snake Game by Jade')
+
 # the quit() is used to unintialise everything at the end of the code
 pygame.quit()
 quit()
+
+
---
+++ textEditior/textEditor.py
@@ -0,0 +1,9 @@
+# this program will function as a text editor where th euse can enter in text, save it , open other files etc
+# author: jade higgins
+# 09/02/2021
+
+from tkinter import *
+
+from tkinter import Tk 
+from tkinter import *
+print('g')
