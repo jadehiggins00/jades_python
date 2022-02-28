82245d8ad2598949c3a4c52cc69f2c20b1e279b2
jade higgins <jadehiggins2000@gmail.com>
2022-02-28 17:22:22 +0100

    We added numpy

--- assignment2.py
+++ assignment2.py
@@ -1,55 +1,24 @@
-# This class is responsibile for all the necessary attributes
-# class sudoku:
-#     def __init__(self):
-#         self.attempts = 0 # counts the number of backtracking
-#         self.cells # this will count the resolved cells of the sudoku grid
-#         self.grid = [0]*81 # sudoku grid which is intialised to cells of 0
-#
-#         self.rowIndex = [] # this will contains 9 indexes of each row
-#         self.colIndex = [] #this will contain 9 indexes of each col
-#         self.sub_grid_Index = [] # contains indexes of each 3x3 sub-grid
-#
-#         for i in range(0, 81, 9):
-#             self.rowIndex.append(range(i, i+9))
+import numpy
 
-# reading the text file that contains 10 sudoku's
-
-class sudoku:
+class Sudoku:
 
     def __init__(self):
-    
+        # we will now create the board
+        self.row = [] # will contain 9 rows
+        self.col = [] # will contain 9 columns
 
 
     def open_text_file(self):
-        f = open("C:\\Users\jadeh\PycharmProjects\Sudoku\Assignment 2 sudoku.txt", "r")
-        # print(f.read())
+        file = open("C:\\Users\jadeh\PycharmProjects\Sudoku\Assignment 2 sudoku.txt", "r")
         list_of_lists = []
-        for line in f:
+        for line in file:
             stripped_line = line.splitlines()
-            #line_list = stripped_line
             list_of_lists.append(stripped_line)
-        f.close()
-        print(list_of_lists)
+        file.close()
 
+        # converting the list to a numpy array
+        arr = numpy.array(list_of_lists)
+        print("array: ", arr)
 
-    def sudoku_board(self):
-
-        nums = []
-        for row in range(3):
-            nums.append([])
-
-            for col in range(1,4):
-                #subgrid = []
-                #for i in range(3):
-                    #for j in range(3):
-                        #subgrid.append([3*row+i][3*col+j])
-                nums[row].append(col)
-
-        print("3x3 grid with numbers")
-       # print(subgrid)
-        print(nums)
-
-#sudoku_board()
-s = sudoku()
-s.sudoku_board()
-#open_text_file()
+s = Sudoku()
+s.open_text_file()
