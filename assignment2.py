dd7f23eef89b0d0d3cf6a7017e426104e1c77612
jade higgins <jadehiggins2000@gmail.com>
2022-03-08 01:40:20 +0100

    making some kind of progress..

--- assignment2.py
+++ assignment2.py
@@ -1,13 +1,10 @@
 import numpy
 
-class Sudoku:
 
 
+from pprint import pprint
 
-    # def __init__(self):
-    #     # we will now create the board
-    #     self.row = [] # will contain 9 rows
-    #     self.col = [] # will contain 9 columns
+
 
 #
 #
@@ -36,61 +33,114 @@ class Sudoku:
 #         pass
 #
 
-    def open_text_file(self):
-        file = open("C:\\Users\jadeh\PycharmProjects\Sudoku\Assignment 2 sudoku.txt", "r")
-        self.list_of_lists = []
-        for line in file:
-            stripped_line = line.splitlines()
-            self.list_of_lists.append(stripped_line)
-        file.close()
 
+
+# this method is used to check if there is the same number in each
+# row and column while also checking each 3x3 grid box
+def possible(list_of_lists,num, row, col):
+    #print('working')
+    # checking the rows if there is the same number in each row
+    for value in range(0,9):
+        if list_of_lists[row][value] == num:
+            return False
+
+    # checking if the number is appearing in the given col
+    for value in range(0,9):
+        if list_of_lists[value][col] == num:
+            return False
+
+    # finding the top left row and col of the 3x3 box
+    row_corner = row - row % 3
+    col_corner = col - col % 3
+    for i in range(3):
+        for j in range(3):
+            if list_of_lists[row_corner + i][col_corner + j] == num:
+                return False
+    return True
+
+def check_cells(list_of_lists):
+    # checking each row
+    for i in range(9):
+        # checking each column
+        for j in range(9):
+            # if the sudoku grid contains 0 in the row
+            if list_of_lists == 0:
+                return i,j
+    # if all fields are filled with numbers between 1-9 return none none
+    return None, None
+
+# we will solve each sudoku puzzle using the backtracking
+# algorithm. each puzzle contained inside a list of lists
+def solve_sudoku(list_of_lists):
+        # checking for the empty fields
+        # for row in range(0,9):
+        #     for col in range(0,9):
+        #         if self.list_of_lists[row][col] == 0:
+        #             for num in range(1,10):
+        #                 if self.possible(row, col, num):
+        #                     self.list_of_lists[row][col] = num
+        #                     self.solve()
+        #                     self.list_of_lists = 0
+        #             return
+        # print(self.list_of_lists)
+
+
+    # Choosing somewhere on the sudoku board to make a guess
+    row, col = check_cells(list_of_lists)
+
+    # if all the fields are filled with numbers between 1-9
+    # then the puzzle has been solved
+    if row is None:
+        return True
+
+    # If there is a cell marked with 0, then we place a value between 1 and 9
+    for value in range(1, 10): # 1 - 9
+        # Now we check if this is a possible value
+        if possible(list_of_lists,value, row, col):
+            # if this is a valid value, then we place on the board
+            list_of_lists[row][col] = value
+            # then we recursively call the function
+            if solve_sudoku(list_of_lists):
+                return True
+
+        # if nothing gets returned true or is not valid value
+        # then we backtrack
+        list_of_lists[row][col] = -1
+
+    # if none of the numbers we tried work, then the sudoku is unsolvable
+    return False
+
+# this method is used to open the text file and put the contents into a list of lists
+if __name__ == '__main__':
+    #file = open("C:\\Users\jadeh\PycharmProjects\Sudoku\Assignment 2 sudoku.txt", "r")
+    file = open("C:\\Users\jadeh\Documents\Python-Projects\jades_python\sudoku1.txt", "r")
+    list_of_lists = []
+    for line in file:
+        stripped_line = line.splitlines()
+        #file.split(",")
+        list_of_lists.append(stripped_line)
+       # list_of_lists.split(",")
+
+    file.close()
+
+    line = 0
+    title = 'Grid'
+    # while(True):
+    #     while title not in list_of_lists and line and line<len(list_of_lists)-1:
+    #         #increment to the next line
+    #         line+=1
+    #         if line == len(list_of_lists)-1:
+    #             print(title+'')
+    #             line+=1
         # converting the list to a numpy array
        # arr = numpy.array(list_of_lists)
 
-        #print("array: ", arr)
-        print(self.list_of_lists)
-
-
-    def possible(self, row, col, num):
-        print('working')
-       # checking the rows if there is the same number in each row
-        for value in range(0,9):
-            if self.list_of_lists[row][value] == num:
-                return False
-
-        # checking if the number is appearing in the given col
-        for value in range(0,9):
-            if self.list_of_lists[value][col] == num:
-                return False
-
-        # finding the top left row and col of the 3x3 box
-        row_corner = row - row % 3
-        col_corner = col - col % 3
-        for i in range(3):
-            for j in range(3):
-                if self.list_of_lists[row_corner + i][col_corner + j] == num:
-                    return False
-        return True
-
-
-    def solve(self):
-        # checking for the empty fields
-        for row in range(0,9):
-            for col in range(0,9):
-                if self.list_of_lists[row][col] == 0:
-                    for num in range(1,10):
-                        if self.possible(row, col, num):
-                            self.list_of_lists[row][col] = num
-                            self.solve()
-                            self.list_of_lists = 0
-                    return
-        print(self.list_of_lists)
-
+    #print("array: ", arr)
+    print(solve_sudoku(list_of_lists))
+    pprint(list_of_lists)
 #
 #
 #
-s = Sudoku()
 
-s.solve()
 # #s.is_valid()
-# s.open_text_file()
+#s.open_text_file()
--- opengl-examples/OpenglCrashCourse/Main.cpp
+++ opengl-examples/OpenglCrashCourse/Main.cpp
@@ -2,16 +2,88 @@
 #include<glad/glad.h>
 #include<GLFW/glfw3.h>
 
+// Vertex Shader source code
+const char* vertexShaderSource = "#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"void main()\n"
+"{\n"
+"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"}\0";
+//Fragment Shader source code
+const char* fragmentShaderSource = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"   FragColor = vec4(0.8f, 0.3f, 0.02f, 1.0f);\n"
+"}\n\0";
+
 int main() {
 
 	/*
 	now we create the window by using GLFWINIT to intilise the window
 	*/
 	glfwInit();
-	// this is a hint for the version of opengl
+	// tell GLFW what version of OPENGL we are using
+	// we are using the newest version
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 4);
-	//glfwWindowHint(GLFW_OPENGL_PROFILE, )
+	// tell GLFW we are using the CORE profile
+	// so that means we only have the modern functions
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLfloat vertices[] =
+	{
+		-0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,
+		0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,
+		0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f
+	};
+
+	// all opengl objects are opened by a reference
+
+	// create a GLFW window object of 800 by 800 pixels
+	// naming it "jadeOpenGl"
+	// error check if the window fails to create
+	GLFWwindow* window = glfwCreateWindow(800, 800, "jadeOpenGl", NULL, NULL);
+	if (window == NULL) {
+		std::cout << "Failed to create GUI window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	// introduce the window into the current context
+	glfwMakeContextCurrent(window); 
+
+	// load GLAD so it configures OpenGl
+	gladLoadGL();
+
+	// specify the viewport of OpenGl in the window
+	// In this case the viewport goes from x=0, y=0 to x=800 y=800
+	glViewport(0, 0, 800, 800);
+
+	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+
+	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+
+	GLuint shaderProgram = glCreateProgram();
+
+	glAttachShader(shaderProgram, vertexShader);
+	// specify the color of the background
+	glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
+	// clean the back buffer and assgn the new color to it
+	glClear(GL_COLOR_BUFFER_BIT);
+	// swap the back buffer with the front buffer
+	glfwSwapBuffers(window);
+	
+	
+	while (!glfwWindowShouldClose(window)) {
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window);
 	// we should also terminate it at the end of the function
 	glfwTerminate();
 	return 0;
